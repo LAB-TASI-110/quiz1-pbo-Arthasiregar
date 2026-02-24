2ab19598432ad06e7a486844bcbc6bf9b79c48b9
Arthasiregar <arthasiregar10@gmail.com>
2026-02-24 13:29:37 +0700

    Update Driver2.java

--- src/driver/Driver2.java
+++ src/driver/Driver2.java
@@ -1,3 +1,26 @@
+package driver;
+
+import java.util.Scanner;
+import model.solution;
+
 public class Driver2 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] nilai = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nilai[i] = sc.nextInt();
+        }
+
+        int kodeKelompok = sc.nextInt();
+
+        solution sol = new solution();
+        int hasil = sol.hitungTotalKelompok(nilai, kodeKelompok);
+
+        System.out.println(hasil);
+    }
 }
+
