e153884ec7d3c05a9f6105bcbc01805b047b594b
Arthasiregar <arthasiregar10@gmail.com>
2026-02-24 13:30:22 +0700

    Update Driver3.java

--- src/driver/Driver3.java
+++ src/driver/Driver3.java
@@ -1,3 +1,39 @@
+package driver;
+
+import java.util.Scanner;
+import model.solution;
+
 public class Driver3 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Nama Mahasiswa : ");
+        String nama = sc.nextLine();
+
+        System.out.print("Masukkan Asrama         : ");
+        String asrama = sc.nextLine();
+
+        System.out.print("Jumlah Baju   : ");
+        int baju = sc.nextInt();
+
+        System.out.print("Jumlah Celana : ");
+        int celana = sc.nextInt();
+
+        System.out.print("Jumlah Jaket  : ");
+        int jaket = sc.nextInt();
+
+        String hasil = solution.prosesLaundry(
+                nama,
+                asrama,
+                baju,
+                celana,
+                jaket
+        );
+
+        System.out.println();
+        System.out.println(hasil);
+
+        sc.close();
+    }
 }
